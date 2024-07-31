3ab877496173fd59407f86e50b3be034bb89f955
Carlos Perales <carlosperales050@gmail.com>
2024-07-31 17:56:38 -0600

    parametros incorrectos

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // activar pipe
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //whitelist quita del payload todos los atributos que no esten en el DTO
+      whitelist: true,
+      // manda una advertencia de atributos extra que no esten en el DTO
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
