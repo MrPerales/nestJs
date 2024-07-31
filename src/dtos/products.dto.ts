8c36a380ed901e0d4a079a7b8a596d33e2451d52
Carlos Perales <carlosperales050@gmail.com>
2024-07-31 17:34:18 -0600

    class validator y mapped types

--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,14 +1,34 @@
+import {
+  IsNumber,
+  IsString,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+// con partialType nos facilitamos de reescribir
+//  todas las validaciones pero van a ser opcionales
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
