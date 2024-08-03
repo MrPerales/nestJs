ae86d2bff826a3532f00e1b2f760b935bcdbe1d0
Carlos Perales <carlosperales050@gmail.com>
2024-08-02 19:42:14 -0600

    useValue

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
+const API_KEY = '123456789';
+const API_KEY_PROD = 'PROD123456';
+
 @Module({
   imports: [ProductsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      // otra forma de injectar servicios o valores
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
