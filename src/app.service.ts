6c2271173c1a1e7da184ec157233ed1bd8bbcb7c
Carlos Perales <carlosperales050@gmail.com>
2024-08-07 17:32:51 -0600

    conf por ambientes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, Inject } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('API_KEY') private apikey: string,
+    // @Inject('API_KEY') private apikey: string,
     @Inject('TASKS') private tasks: any[],
+    private config: ConfigService,
   ) {}
   // forma segura de inyectar un valor que queremos
   //  que se use atra ves de toda la app
   getHello(): string {
-    console.log(this.tasks);
-    return `Hello World! ${this.apikey}`;
+    const apikey = this.config.get('API_KEY');
+    const name = this.config.get('DATABASE_NAME');
+
+    return `Hello World! ${apikey} ${name}`;
   }
 }
