0d3e3f35f1a3786f90ecf369805400fd874966cf
Carlos Perales <carlosperales050@gmail.com>
2024-08-12 18:26:11 -0600

    conection basic mysql

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,16 @@
 import { Module, Global } from '@nestjs/common';
+import * as mysql from 'mysql2';
+// const mysql = require('mysql2');
 
 const API_KEY = '123456789';
 const API_KEY_PROD = 'PROD123456';
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: '',
+  database: '',
+  password: '',
+});
 
 @Global()
 @Module({
@@ -11,10 +20,15 @@ const API_KEY_PROD = 'PROD123456';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      // otra forma de injectar servicios o valores
+      provide: 'MYSQL',
+      useValue: pool,
+    },
   ],
   // con esto se va a poder utilizar desde cualquier modulo
   // y como es global no necesitamos importalo solo lo injectamos
   // "@Inject('API_KEY')"
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'MYSQL'],
 })
 export class DatabaseModule {}
