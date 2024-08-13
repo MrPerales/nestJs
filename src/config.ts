119d289ca3890f2458b99ae8ec20fb0d229299bb
Carlos Perales <carlosperales050@gmail.com>
2024-08-12 19:21:36 -0600

    agregando variables de ambiente

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,14 @@ export default registerAs('config', () => {
       name: process.env.DATABASE_NAME,
       port: process.env.DATABASE_PORT,
     },
+    mysql: {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      databaseName: process.env.DB_NAME,
+      port: parseInt(process.env.DB_PORT, 10),
+    },
+
     apiKey: process.env.API_KEY,
   };
 });
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,10 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import * as mysql from 'mysql2';
-// const mysql = require('mysql2');
+import config from 'src/config';
 
 const API_KEY = '123456789';
 const API_KEY_PROD = 'PROD123456';
-
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: '',
-  database: '',
-  password: '',
-});
 
 @Global()
 @Module({
@@ -23,7 +17,17 @@ const pool = mysql.createPool({
     {
       // otra forma de injectar servicios o valores
       provide: 'MYSQL',
-      useValue: pool,
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, databaseName, password, host } = configService.mysql;
+        const pool = mysql.createPool({
+          host,
+          user,
+          database: databaseName,
+          password,
+        });
+        return pool;
+      },
+      inject: [config.KEY],
     },
   ],
   // con esto se va a poder utilizar desde cualquier modulo
