9d3c5dba53a095a27f62378e47b744079b4d084e
Carlos Perales <carlosperales050@gmail.com>
2024-08-12 18:27:30 -0600

    peticion a la database

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,4 +20,9 @@ export class AppController {
   hello() {
     return 'con /sas/';
   }
+
+  @Get('/tasks')
+  getTasks() {
+    return this.appService.getTasks();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import config from './config';
+import { RowDataPacket } from 'mysql2';
 
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apikey: string,
+    @Inject('MYSQL') private pool: RowDataPacket,
     @Inject('TASKS') private tasks: any[],
     // para tipar las .env
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
@@ -17,5 +19,14 @@ export class AppService {
     const name = this.configService.database.name;
 
     return `Hello World! ${apikey} ${name}`;
+  }
+
+  getTasks() {
+    return new Promise((resove, reject) => {
+      this.pool.query('SELECT * FROM tasks', (err, rows, fields) => {
+        if (err) reject(err);
+        resove(rows);
+      });
+    });
   }
 }
