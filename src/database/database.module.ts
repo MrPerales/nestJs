c5a519e1e94e779b23f4c8273bcdf57bb4216bae
Carlos Perales <carlosperales050@gmail.com>
2024-08-13 17:22:22 -0600

    config typeORM

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,12 +2,31 @@ import { Module, Global } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import * as mysql from 'mysql2';
 import config from 'src/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const API_KEY = '123456789';
 const API_KEY_PROD = 'PROD123456';
 
 @Global()
 @Module({
+  imports: [
+    // servicio orm
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { host, user, databaseName, password } = configService.mysql;
+
+        return {
+          type: 'mysql',
+          host,
+          username: user,
+          password,
+          database: databaseName,
+          synchronize: true,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       // otra forma de injectar servicios o valores
@@ -33,6 +52,6 @@ const API_KEY_PROD = 'PROD123456';
   // con esto se va a poder utilizar desde cualquier modulo
   // y como es global no necesitamos importalo solo lo injectamos
   // "@Inject('API_KEY')"
-  exports: ['API_KEY', 'MYSQL'],
+  exports: ['API_KEY', 'MYSQL', TypeOrmModule],
 })
 export class DatabaseModule {}
