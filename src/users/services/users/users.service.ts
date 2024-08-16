f5d9136c279db9c9c21cfe1ef93cb87f19679b41
Carlos Perales <carlosperales050@gmail.com>
2024-08-15 18:19:51 -0600

    peticiones con TypeORM en UsersService

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,73 +1,57 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ProductsService } from 'src/products/services/products/products.service';
 import { CreateUserDto, UpdateUserdto } from 'src/users/dtos/users.dto';
-import { Order } from 'src/users/entities/order.entity';
 import { User } from 'src/users/entities/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
   constructor(
+    @InjectRepository(User) private userRepo: Repository<User>,
     private productsService: ProductsService,
     // solo se va a poder usar gracias al modulo global
     private configService: ConfigService,
   ) {}
 
-  private counterId = 1;
-  private users: User[] = [
-    {
-      id: 1,
-      name: 'Name',
-      lastName: 'LastName',
-      nickname: 'ElName',
-      password: 'password',
-      image: 'adhdsio',
-      mail: 'sdakd@mail.com',
-    },
-  ];
   fndAll() {
-    const apikey = this.configService.get('API_KEY');
-    const dataname = this.configService.get('DATABASE_NAME');
-    console.log('apikey:', apikey);
-    console.log('DATABASENAME:', dataname);
-    return this.users;
+    // examples
+    // const apikey = this.configService.get('API_KEY');
+    // const dataname = this.configService.get('DATABASE_NAME');
+    // console.log('apikey:', apikey);
+    // console.log('DATABASENAME:', dataname);
+    return this.userRepo.find();
   }
-  findOne(id: User['id']) {
-    const user = this.users.find((item) => item.id === id);
+  async findOne(id: User['id']) {
+    const user = await this.userRepo.findOneBy({ id });
     if (!user) {
       throw new NotFoundException(`user #${id} not found `);
     }
     return user;
   }
+
   create(payload: CreateUserDto) {
-    this.counterId += 1;
-    const newUser = {
-      id: this.counterId,
-      ...payload,
-    };
-    this.users.push(newUser);
-    return newUser;
+    const newUser = this.userRepo.create(payload);
+    return this.userRepo.save(newUser);
   }
-  update(id: User['id'], payload: UpdateUserdto) {
-    const user = this.findOne(id);
+
+  async update(id: User['id'], payload: UpdateUserdto) {
+    const user = await this.findOne(id);
     if (user) {
-      const index = this.users.findIndex((item) => item.id === id);
-      this.users[index] = {
-        ...user,
-        ...payload,
-        id,
-      };
-      return this.users[index];
+      this.userRepo.merge(user, payload);
+      return this.userRepo.save(user);
     }
     return null;
   }
-  delete(id: User['id']) {
-    const index = this.users.findIndex((item) => item.id === id);
-    if (index === -1) {
-      throw new NotFoundException(`user #${id} not found `);
+
+  async delete(id: User['id']) {
+    const user = await this.findOne(id);
+    if (user) {
+      this.userRepo.delete(id);
+      return { message: 'deleted', body: { user } };
     }
-    this.users.splice(index, 1);
-    return { message: 'deleted' };
+    return null;
   }
 
   async getOrdersByUser(id: User['id']) {
