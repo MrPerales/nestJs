b739871b1156b2dfc943f87caa9fe3936c6ae81f
Carlos Perales <carlosperales050@gmail.com>
2024-10-24 12:22:35 -0600

    add : orderItem endpoints (update,delete)

--- src/users/controllers/order-item/order-item.controller.ts
+++ src/users/controllers/order-item/order-item.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { CreateOrderItemDto } from 'src/users/dtos/order-item.dto';
+import {
+  CreateOrderItemDto,
+  UpdateOrderItemDto,
+} from 'src/users/dtos/order-item.dto';
 import { OrderItemService } from 'src/users/services/order-item/order-item.service';
 
 @ApiTags()
@@ -12,5 +23,20 @@ export class OrderItemController {
   @Post()
   create(@Body() payload: CreateOrderItemDto) {
     return this.orderItemService.create(payload);
+  }
+
+  @ApiOperation({ summary: 'update order' })
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateOrderItemDto,
+  ) {
+    return this.orderItemService.update(id, payload);
+  }
+
+  @ApiOperation({ summary: 'delete order' })
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.orderItemService.delete(id);
   }
 }
--- src/users/services/order-item/order-item.service.ts
+++ src/users/services/order-item/order-item.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from '../../../products/entities/product.entity';
 
@@ -18,6 +18,14 @@ export class OrderItemService {
     @InjectRepository(Product) private productRepo: Repository<Product>,
   ) {}
 
+  async findOne(id: OrderItem['id']) {
+    const order = await this.orderItemRepo.findOneBy({ id });
+    if (!order) {
+      throw new NotFoundException(`Order #${id} not found`);
+    }
+    return order;
+  }
+
   async create(payload: CreateOrderItemDto) {
     // obtenemos la orden y el producto al que va relacionado
     const order = await this.orderRepo.findOneBy({ id: payload.orderId });
@@ -27,5 +35,37 @@ export class OrderItemService {
     item.product = product;
     item.quantity = payload.quantity;
     return this.orderItemRepo.save(item);
+  }
+
+  async update(id: OrderItem['id'], payload: UpdateOrderItemDto) {
+    const orderItem = await this.findOne(id);
+    console.log(orderItem);
+
+    if (payload.orderId) {
+      const order = await this.orderRepo.findOneBy({ id: payload.orderId });
+      orderItem.order = order;
+    }
+    if (payload.productId) {
+      const product = await this.productRepo.findOneBy({
+        id: payload.productId,
+      });
+      orderItem.product = product;
+    }
+    if (payload.quantity) {
+      orderItem.quantity = payload.quantity;
+    }
+    // se puede usar merge pero como ya asignamos todas las propiedades no tiene caso
+    // this.orderItemRepo.merge(orderItem, payload);
+
+    return this.orderItemRepo.save(orderItem);
+  }
+
+  async delete(id: number) {
+    const orderItem = await this.findOne(id);
+    if (orderItem) {
+      this.orderRepo.delete(id);
+      return { message: 'deleted', body: { orderItem } };
+    }
+    return null;
   }
 }
