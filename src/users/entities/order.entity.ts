d5c5a0846c8be07d890541dfe1f79d2e5ffe2f9c
Carlos Perales <carlosperales050@gmail.com>
2024-10-25 18:59:03 -0600

    serializacion expose

--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -8,7 +8,7 @@ import {
 } from 'typeorm';
 import { Customer } from './customer.entity';
 import { OrderItem } from './order-item.entity';
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 
 @Entity({ name: 'orders' })
 export class Order {
@@ -35,6 +35,38 @@ export class Order {
   @JoinColumn({ name: 'customer_id' })
   customer: Customer;
 
+  @Exclude()
   @OneToMany(() => OrderItem, (item) => item.order)
   items: OrderItem[];
+
+  // campo nuevo funciona con los geters
+  @Expose()
+  //el campo va a aparecer como products
+  get products() {
+    // aseguramos de que hay items
+    if (this.items) {
+      //!!item => item !== null o undefine
+      return this.items
+        .filter((item) => !!item) //aseguramos que no este un null o undefined
+        .map((item) => ({
+          ...item.product,
+          quantity: item.quantity,
+          orderItemId: item.id,
+        }));
+    }
+    return [];
+  }
+
+  @Expose()
+  get total() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item)
+        .reduce((total, item) => {
+          const totalItem = item.product.price * item.quantity;
+          return total + totalItem;
+        }, 0);
+    }
+    return 0;
+  }
 }
